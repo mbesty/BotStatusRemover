d90d15f350d47e075cbfcbed0d1f499e6f26b0b1
mbesty <158783020+mbesty@users.noreply.github.com>
2025-01-08 23:36:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Listening to Use Code Mbestie 💖", "Listening to Use Code Mbestie❤️"];
+const statusMessages = ["💖Use Code Mbestie", "❤️Use Code Mbestie"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
